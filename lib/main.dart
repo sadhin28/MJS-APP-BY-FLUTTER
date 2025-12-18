8326e0cc89ec50432656c89322f03b22338f13a7
sadhin28 <taosifsadhin113@gmail.com>
2025-12-19 01:16:56 +0600

    add expanded widgets

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,11 @@ class MyApp extends StatelessWidget {
               height: 200.0,
               color: Colors.red,
             ), ),
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 200.0,
+              color: Colors.green,
+            ), ),
           ],
         ),
       ),
