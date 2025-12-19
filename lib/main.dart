f78ca165a5d7fde7cef4c06d2122b584020ef33d
sadhin28 <taosifsadhin113@gmail.com>
2025-12-20 01:20:28 +0600

    remove green box

--- lib/main.dart
+++ lib/main.dart
@@ -52,12 +52,7 @@ class MyApp extends StatelessWidget {
               color: Colors.green,
 
             ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 200.0,
-              color: Colors.red,
 
-            ), ),
 
           ],
         ),
