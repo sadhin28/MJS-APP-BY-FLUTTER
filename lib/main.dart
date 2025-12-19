ba007743c640078b75de5fa185fbb301721d39f5
sadhin28 <taosifsadhin113@gmail.com>
2025-12-20 01:21:57 +0600

    remove blue color

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,6 @@ class MyApp extends StatelessWidget {
             Expanded(child:Container(
               width: double.infinity,
               height: 700.0,
-              color: Colors.blue,
-            ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 700.0,
               color: Colors.green,
             ), ),
             Expanded(child:Container(
