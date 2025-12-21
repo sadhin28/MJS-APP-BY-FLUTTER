211c11e2195aac36aeded6ab59a179381b8b5824
sadhin28 <taosifsadhin113@gmail.com>
2025-12-22 01:04:49 +0600

    add blue card

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,12 @@ class MyApp extends StatelessWidget {
               color: Colors.red,
             ), ),
 
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 200.0,
+              color: Colors.lightBlue,
+            ), ),
+
 
 
 
