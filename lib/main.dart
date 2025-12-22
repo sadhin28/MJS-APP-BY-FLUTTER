92ea3618abc51beb1c85a074b2f72d6ad98b6bd1
sadhin28 <taosifsadhin113@gmail.com>
2025-12-22 22:43:55 +0600

    add red container

--- lib/main.dart
+++ lib/main.dart
@@ -39,16 +39,7 @@ class MyApp extends StatelessWidget {
               height: 200.0,
               color: Colors.lightBlue,
             ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 200.0,
-              color: Colors.black,
-            ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 200.0,
-              color: Colors.red,
-            ), ),
+
 
 
 
