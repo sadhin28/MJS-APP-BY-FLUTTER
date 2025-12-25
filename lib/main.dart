51d8f8a482d5e629df2c0648403a8229ad9a6229
sadhin28 <taosifsadhin113@gmail.com>
2025-12-26 02:19:38 +0600

    add expanded box

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,6 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.green,
             ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 200.0,
-              color: Colors.red,
-            ), ),
 
           ],
         ),
