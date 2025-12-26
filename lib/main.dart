a3cf2856a9898f029b4223d351723bb038e32dcc
sadhin28 <taosifsadhin113@gmail.com>
2025-12-27 02:21:29 +0600

    delete widgets

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,12 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.red,
             ), ),
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.green,
+            ), ),
+
           ],
         ),
       ),
