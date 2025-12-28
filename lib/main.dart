5b910870e5d3c97bbe9915093adf676550eabb67
sadhin28 <taosifsadhin113@gmail.com>
2025-12-28 22:24:16 +0600

    add green card

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,6 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.red,
             ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 700.0,
-              color: Colors.green,
-            ), ),
 
           ],
         ),
