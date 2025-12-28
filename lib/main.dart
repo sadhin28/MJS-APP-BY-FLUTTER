a6a9e618f0bc0ccb72621f2b0555b03fb04d113e
sadhin28 <taosifsadhin113@gmail.com>
2025-12-29 01:00:20 +0600

    add green and red box int the home page

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,17 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.red,
             ), ),
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.green,
+            ), ),
+
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.red,
+            ), ),
 
           ],
         ),
