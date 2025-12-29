e475936b2c21f19c374f8ff294024f941775291b
sadhin28 <taosifsadhin113@gmail.com>
2025-12-30 01:50:01 +0600

    add green box

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,11 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.green,
             ), ),
-
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.green,
+            ), ),
 
           ],
         ),
