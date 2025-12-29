66ade5667ec4c7cea029b7f8ca9a5d15964cc8bf
sadhin28 <taosifsadhin113@gmail.com>
2025-12-30 01:49:31 +0600

    remove red box

--- lib/main.dart
+++ lib/main.dart
@@ -55,11 +55,6 @@ class MyApp extends StatelessWidget {
               color: Colors.green,
             ), ),
 
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 700.0,
-              color: Colors.red,
-            ), ),
 
           ],
         ),
