d13c1d78356011e2ed7657d5e91ced5caed9bdf1
sadhin28 <taosifsadhin113@gmail.com>
2026-01-01 00:23:26 +0600

    add new box

--- lib/main.dart
+++ lib/main.dart
@@ -33,11 +33,7 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.red,
             ), ),
-            Expanded(child:Container(
-              width: double.infinity,
-              height: 700.0,
-              color: Colors.green,
-            ), ),
+
 
             Expanded(child:Container(
               width: double.infinity,
