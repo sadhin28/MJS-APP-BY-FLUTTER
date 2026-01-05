0a25587f8fd7ba8245b4496dd5195db7c7dc0d45
sadhin28 <taosifsadhin113@gmail.com>
2026-01-05 19:21:26 +0600

    add red and blue box

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
             Expanded(child:Container(
               width: double.infinity,
               height: 700.0,
-              color: Colors.green,
+              color: Colors.red,
             ), ),
 
             Expanded(child:Container(
