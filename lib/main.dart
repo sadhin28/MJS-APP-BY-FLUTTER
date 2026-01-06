4e649e3c4c61910428e8e40cbe541278efcb0a09
sadhin28 <taosifsadhin113@gmail.com>
2026-01-06 17:44:20 +0600

    add red child

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,11 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.yellow,
             ), ),
-
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.red,
+            ), ),
 
 
 
