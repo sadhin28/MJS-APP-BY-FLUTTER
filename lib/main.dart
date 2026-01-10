696afdabd81b9d2d900301c114e285587c82dbaf
sadhin28 <taosifsadhin113@gmail.com>
2026-01-10 22:11:00 +0600

    add child

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,11 @@ class MyApp extends StatelessWidget {
               color: Colors.red,
             ), ),
 
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.blue,
+            ), ),
           ],
         ),
       ),
