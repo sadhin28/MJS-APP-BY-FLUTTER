6f760c76d83d90293fef16cbf04a2585fa0972db
sadhin28 <taosifsadhin113@gmail.com>
2026-01-13 18:57:03 +0600

    add pink color

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyApp extends StatelessWidget {
             Expanded(child:Container(
               width: double.infinity,
               height: 700.0,
-              color: Colors.red,
+              color: Colors.pink,
             ), ),
           ],
         ),
