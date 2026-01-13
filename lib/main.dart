97e0d86da21c99b6f2c7966915c37bd7b1ed00ab
sadhin28 <taosifsadhin113@gmail.com>
2026-01-13 18:57:52 +0600

    add red card for image

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,12 @@ class MyApp extends StatelessWidget {
               height: 700.0,
               color: Colors.pink,
             ), ),
+
+            Expanded(child:Container(
+              width: double.infinity,
+              height: 700.0,
+              color: Colors.blueAccent,
+            ), ),
           ],
         ),
       ),
